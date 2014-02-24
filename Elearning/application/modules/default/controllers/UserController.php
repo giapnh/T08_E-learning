272410d5714223b19e6f812f5f694a0e5ed3a55b
giapnh <hgbk.it@gmail.com>
2014-02-24 15:41:59 +0700

    Fix SQL design

--- Elearning/application/modules/default/controllers/UserController.php
+++ Elearning/application/modules/default/controllers/UserController.php
@@ -1,149 +1,153 @@
 <?php
+
 require 'IController.php';
+
 class UserController extends IController {
-	public function indexAction(){
-		$this->redirect('user/login');
-	}
 
-	public function loginAction() {
-		// 		//Ip cache
-		// 		$frontendOptions = array(
-		// 				'lifetime' => Code::$LOGIN_FAIL_LOCK_TIME, // cache lifetime of 2 hours
-		// 				'automatic_serialization' => true
-		// 		);
+    public function indexAction() {
+        $this->redirect('user/login');
+    }
 
-		// 		$backendOptions = array(
-		// 				'cache_dir' => 'C:/Users/Public' // Directory where to put the cache files
-		// 		);
+    public function loginAction() {
+        // 		//Ip cache
+        // 		$frontendOptions = array(
+        // 				'lifetime' => Code::$LOGIN_FAIL_LOCK_TIME, // cache lifetime of 2 hours
+        // 				'automatic_serialization' => true
+        // 		);
+        // 		$backendOptions = array(
+        // 				'cache_dir' => 'C:/Users/Public' // Directory where to put the cache files
+        // 		);
+        // 		$cache = Zend_Cache::factory('Core',
+        // 				'File',
+        // 				$frontendOptions,
+        // 				$backendOptions);
+        // 		// see if a cache already exists:
+        // 		if($result = $cache->load('ip_locking')) {
+        // 			// 			$cache->save($result, 'myresult');
+        // 		} else {
+        // 			// cache hit! shout so that we know
+        // 			echo "This one is from cache!\n\n";
+        // 		}
+        $form = new Default_Form_Login ();
+        $this->view->form = $form;
+        if ($this->_request->isPost()) {
+            $authAdapter = new Default_Model_Account();
+            $auth = Zend_Auth::getInstance();
+            $uname = $this->_request->getParam('username');
+            $paswd = $this->_request->getParam('password');
+            $role = $this->_request->getParam('role');
+            // If filed is null
+            if (trim($uname) == '') {
+                $this->view->errorMessage = Message::$M001;
+                return;
+            } else if (trim($paswd) == '') {
+                $this->view->errorMessage = Message::$M002;
+                return;
+            }
 
-		// 		$cache = Zend_Cache::factory('Core',
-		// 				'File',
-		// 				$frontendOptions,
-		// 				$backendOptions);
-		// 		// see if a cache already exists:
-		// 		if($result = $cache->load('ip_locking')) {
-			
-		// 			// 			$cache->save($result, 'myresult');
-		// 		} else {
-		// 			// cache hit! shout so that we know
-		// 			echo "This one is from cache!\n\n";
-		// 		}
-		$form = new Default_Form_Login ();
-		$this->view->form = $form;
-		if ($this->_request->isPost ()) {
-			$authAdapter = new Default_Model_Account();
-			$auth = Zend_Auth::getInstance();
-			$uname = $this->_request->getParam ( 'username' );
-			$paswd = $this->_request->getParam ( 'password' );
-			$role = $this->_request->getParam('role');
-			// If filed is null
-			if(trim($uname) == ''){
-				$this->view->errorMessage = Message::$M001;
-				return;
-			}else if(trim($paswd) == ''){
-				$this->view->errorMessage = Message::$M002;
-				return;
-			}
+            $flag = false;
+            if ($authAdapter->isValid($uname, $paswd, $role)) {
+                $curr_ip = $_SERVER['REMOTE_ADDR'];
+                if ($curr_ip == "::1") {
+                    $curr_ip = "127.0.0.1";
+                }
 
-			$flag = false;
-			if ($authAdapter->isValid($uname,$paswd,$role)) {
-				$curr_ip =  $_SERVER['REMOTE_ADDR'];
-				if($curr_ip == "::1"){
-					$curr_ip = "127.0.0.1";
-				}
-				if($authAdapter->checkIpValid($uname, $curr_ip)){
-					// 					$data = $authAdapter->getResultRowObject ( null, array (
-					// 							'password'
-					// 					) );
-					// 					$auth->getStorage ()->write ( $data );
-					$flag = true;
-				}else{
-					echo "Invalid ip address";
+                // If user is teacher, check ip address valid or invalid
+                if ($role == 2) {
+                    if ($authAdapter->checkIpValid($uname, $curr_ip)) {
+                        // 					$data = $authAdapter->getResultRowObject ( null, array (
+                        // 							'password'
+                        // 					) );
+                        // 					$auth->getStorage ()->write ( $data );
+                        $flag = true;
+                    } else {
+                        echo "Invalid ip address";
 
-					$flag = false;
-				}
-			}else{
-				$authAdapter->incLoginFailure($uname);
-				$this->view->errorMessage = Message::$M003;
-			}
-			if ($flag == true) {
-				if($role == 1){
-					$this->_redirect ( '/student/index' );
-				}else{
-					$this->_redirect ( '/teacher/index' );
-				}
-			}
-		}
-	}
+                        $flag = false;
+                    }
+                }
+            } else {
+                $authAdapter->incLoginFailure($uname);
+                $this->view->errorMessage = Message::$M003;
+            }
+            if ($flag == true) {
+                if ($role == 1) {
+                    $this->_redirect('/student/index');
+                } else {
+                    $this->_redirect('/teacher/index');
+                }
+            }
+        }
+    }
 
-	public function registerAction(){
-		$user = new Default_Model_Account();
-		$form = new Default_Form_Register ();
-		$this->view->form = $form;
-		if ($this->_request->isPost ()) {
-			$data = $this->_request->getParams();
-			//Check empty
-			if(trim($data['username'])==''){
-				$this->view->errorMessage = Message::$M006;
-				return;
-			}
-			if(trim($data['password'])==''){
-				$this->view->errorMessage = Message::$M007;
-				return;
-			}
+    public function registerAction() {
+        $user = new Default_Model_Account();
+        $form = new Default_Form_Register ();
+        $this->view->form = $form;
+        if ($this->_request->isPost()) {
+            $data = $this->_request->getParams();
+            //Check empty
+            if (trim($data['username']) == '') {
+                $this->view->errorMessage = Message::$M006;
+                return;
+            }
+            if (trim($data['password']) == '') {
+                $this->view->errorMessage = Message::$M007;
+                return;
+            }
 
-			if($data['password'] != $data['repassword']){
-				$this->view->errorMessage = Message::$M008;
-				return;
-			}
+            if ($data['password'] != $data['repassword']) {
+                $this->view->errorMessage = Message::$M008;
+                return;
+            }
 
-			if(trim($data['fullname'])==''){
-				$this->view->errorMessage = Message::$M009;
-				return;
-			}
+            if (trim($data['fullname']) == '') {
+                $this->view->errorMessage = Message::$M009;
+                return;
+            }
 
-			if($data['day'] == 0 || $data['month'] == 0 || $data['year'] == 0){
-				$this->view->errorMessage = Message::$M010;
-				return;
-			}
+            if ($data['day'] == 0 || $data['month'] == 0 || $data['year'] == 0) {
+                $this->view->errorMessage = Message::$M010;
+                return;
+            }
 
-			if(trim($data['address']) == ''){
-				$this->view->errorMessage = Message::$M011;
-				return;
-			}
+            if (trim($data['address']) == '') {
+                $this->view->errorMessage = Message::$M011;
+                return;
+            }
 
-			if(trim($data['phone']) == ''){
-				$this->view->errorMessage = Message::$M012;
-				return;
-			}
+            if (trim($data['phone']) == '') {
+                $this->view->errorMessage = Message::$M012;
+                return;
+            }
 
-			if(trim($data['bank_acc'])==''){
-				$this->view->errorMessage = Message::$M013;
-			}
+            if (trim($data['bank_acc']) == '') {
+                $this->view->errorMessage = Message::$M013;
+            }
 
-			if(trim($data['secret_question'])==''){
-				$this->view->errorMessage = Message::$M014;
-				return;
-			}
+            if (trim($data['secret_question']) == '') {
+                $this->view->errorMessage = Message::$M014;
+                return;
+            }
 
-			if(trim($data['secret_answer'])==''){
-				$this->view->errorMessage = Message::$M015;
-				return;
-			}
+            if (trim($data['secret_answer']) == '') {
+                $this->view->errorMessage = Message::$M015;
+                return;
+            }
 
-			if($user->isExits($data['username'])){
-				$this->view->errorMessage = Message::$M034;
-				return;
-			}
-                        $user->insertNew($data);
-			$this->_redirect('user/login');
-		}
-			
-	}
+            if ($user->isExits($data['username'])) {
+                $this->view->errorMessage = Message::$M034;
+                return;
+            }
+            $user->insertNew($data);
+            $this->_redirect('user/login');
+        }
+    }
 
-	public function logoutAction(){
-		$auth = Zend_Auth::getInstance();
-		$auth->clearIdentity();
-		$this->_redirect('user/login');
-	}
+    public function logoutAction() {
+        $auth = Zend_Auth::getInstance();
+        $auth->clearIdentity();
+        $this->_redirect('user/login');
+    }
+
 }
