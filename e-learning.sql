726e310a257704b6d0334474ae0836322c1f7436
giapnh <hgbk.it@gmail.com>
2014-02-24 15:43:20 +0700

    No change

--- e-learning.sql
+++ e-learning.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.5
+-- version 4.0.4.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Feb 19, 2014 at 05:42 AM
--- Server version: 5.5.16
--- PHP Version: 5.3.8
+-- Host: 127.0.0.1
+-- Generation Time: Feb 24, 2014 at 09:42 AM
+-- Server version: 5.5.32
+-- PHP Version: 5.4.16
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,239 +17,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `e-learning`
+-- Database: `elearning`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
-
-CREATE TABLE IF NOT EXISTS `admin` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `create_admin` int(11) NOT NULL,
-  `status` int(11) NOT NULL,
-  `fail_login` int(11) NOT NULL,
-  `log_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin_ip`
---
-
-CREATE TABLE IF NOT EXISTS `admin_ip` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `admin_id` int(11) NOT NULL,
-  `ip` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `comment`
---
-
-CREATE TABLE IF NOT EXISTS `comment` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `lesson_id` int(11) NOT NULL,
-  `comment` varchar(512) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `copyright_report`
---
-
-CREATE TABLE IF NOT EXISTS `copyright_report` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `file_id` int(11) NOT NULL,
-  `reason` varchar(255) NOT NULL,
-  `status` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `file_comment`
---
-
-CREATE TABLE IF NOT EXISTS `file_comment` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `file_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `comment` varchar(512) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `learn`
---
-
-CREATE TABLE IF NOT EXISTS `learn` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `student_id` int(11) NOT NULL,
-  `lesson_id` int(11) NOT NULL,
-  `register_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `status` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `lesson`
---
-
-CREATE TABLE IF NOT EXISTS `lesson` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `teacher_id` int(11) NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `description` varchar(512) NOT NULL,
-  `view` int(11) NOT NULL,
-  `like` int(11) NOT NULL,
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `status` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `lesson_file`
---
-
-CREATE TABLE IF NOT EXISTS `lesson_file` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `lesson_id` int(11) NOT NULL,
-  `filename` varchar(255) NOT NULL,
-  `description` varchar(512) NOT NULL,
-  `location` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `lesson_tag`
---
-
-CREATE TABLE IF NOT EXISTS `lesson_tag` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `lesson_id` int(11) NOT NULL,
-  `tag_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `like`
---
-
-CREATE TABLE IF NOT EXISTS `like` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `lesson_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `question_answer_list`
---
-
-CREATE TABLE IF NOT EXISTS `question_answer_list` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `question_id` int(11) NOT NULL,
-  `answer` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tag`
---
-
-CREATE TABLE IF NOT EXISTS `tag` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tag_name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `test_detail`
---
-
-CREATE TABLE IF NOT EXISTS `test_detail` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `file_id` int(11) NOT NULL,
-  `comment` varchar(512) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `subtitle` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `test_question`
---
-
-CREATE TABLE IF NOT EXISTS `test_question` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `file_id` int(11) NOT NULL,
-  `question` varchar(512) NOT NULL,
-  `true` varchar(5) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `test_result`
---
-
-CREATE TABLE IF NOT EXISTS `test_result` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `learn_id` int(11) NOT NULL,
-  `test_file_id` int(11) NOT NULL,
-  `point` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `test_user_answer`
---
-
-CREATE TABLE IF NOT EXISTS `test_user_answer` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `test_result_id` int(11) NOT NULL,
-  `test_question_id` int(11) NOT NULL,
-  `selected` varchar(5) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -259,25 +28,35 @@ CREATE TABLE IF NOT EXISTS `test_user_answer` (
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` int(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `first_password` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `sex` int(1) NOT NULL DEFAULT '0' COMMENT '0: boy, 1:girl',
+  `email` varchar(100) DEFAULT NULL,
   `birthday` date NOT NULL,
-  `adress` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
   `phone` varchar(20) NOT NULL,
-  `bank_acount` varchar(20) NOT NULL,
+  `bank_account` varchar(30) NOT NULL,
   `first_secret_question` varchar(255) NOT NULL,
   `first_secret_answer` varchar(255) NOT NULL,
   `secret_question` varchar(255) NOT NULL,
   `secret_answer` varchar(255) NOT NULL,
-  `role` int(11) NOT NULL,
+  `role` int(11) NOT NULL DEFAULT '1' COMMENT '1: student,2:teacher',
   `status` int(11) NOT NULL DEFAULT '1',
-  `last_login_ip` varchar(255) NOT NULL,
-  `fail_login` int(11) NOT NULL,
-  `log_time` int(11) NOT NULL,
+  `last_login_ip` varchar(255) DEFAULT NULL,
+  `fail_login_count` int(11) NOT NULL DEFAULT '0',
+  `last_login_time` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `first_password`, `password`, `name`, `sex`, `email`, `birthday`, `address`, `phone`, `bank_account`, `first_secret_question`, `first_secret_answer`, `secret_question`, `secret_answer`, `role`, `status`, `last_login_ip`, `fail_login_count`, `last_login_time`) VALUES
+(1, 'giapnh', '7f8d226668282f0dde3f6dcb54996acf', '7f8d226668282f0dde3f6dcb54996acf', 'Giap', 0, NULL, '0000-00-00', 'Hoang Mai', '01664643234', '711A948594898', '1+1=', '2', '1+1=', '2', 1, 2, NULL, 0, NULL),
+(2, 'huugiap', '7f8d226668282f0dde3f6dcb54996acf', '7f8d226668282f0dde3f6dcb54996acf', 'Giap', 0, NULL, '0000-00-00', 'Hoang Mai', '01664643234', '711A948594898', '1+1=', '2', '1+1=', '2', 1, 2, NULL, 0, NULL);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
