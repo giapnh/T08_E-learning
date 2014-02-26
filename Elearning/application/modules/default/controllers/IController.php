3d74833ccf93fa113e1d1a0e9c7ea230b5d7fc6a
giapnh <hgbk.it@gmail.com>
2014-02-26 17:03:52 +0700

    Student home page(html)

--- Elearning/application/modules/default/controllers/IController.php
+++ Elearning/application/modules/default/controllers/IController.php
@@ -1,11 +1,16 @@
 <?php
+
 require 'Message.php';
+
 class IController extends Zend_Controller_Action {
-	public function init() {
-		$baseurl = $this->_request->getbaseurl ();
-		$this->view->doctype ();
-		$this->view->headtitle ( "E-learning" );
-		$this->view->headMeta ()->appendName ( "keyword", "E-learning..." );
-		$this->view->headLink ()->appendStylesheet ( $baseurl . "/public/css/style.css" );
-	}
+
+    public function init() {
+        $baseurl = $this->_request->getbaseurl();
+        $this->view->doctype();
+        $this->view->headtitle("E-learning");
+        $this->view->headMeta()->appendName("keyword", "E-learning...");
+        $this->view->headLink()->appendStylesheet($baseurl . "/public/css/style.css");
+        $this->view->headLink()->appendStylesheet($baseurl . "/public/css/style_2.css");
+    }
+
 }
