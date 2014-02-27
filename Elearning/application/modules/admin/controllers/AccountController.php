c97c9483151b4f65291a80b5f83035b8d1c9e6aa
tminhhp <tminhhp@gmail.com>
2014-02-28 03:30:43 +0700

    Screen design 4.1 to 4.7

--- Elearning/application/modules/admin/controllers/AccountController.php
+++ Elearning/application/modules/admin/controllers/AccountController.php
@@ -4,14 +4,15 @@ require 'IController.php';
 
 class Admin_AccountController extends IController {
 
+    public function init(){
+        parent::init();
+    }
+    
     /**
      * 管理者の個人情報画面
      */
     public function indexAction() {
-//            $muser=new Default_Model_Account;
-//            $data=$muser->listAll();
-//            print_r($data);
-        echo 'Account Controller, index action';
+        
     }
 
     /**
--- Elearning/application/modules/admin/controllers/UserController.php
+++ Elearning/application/modules/admin/controllers/UserController.php
@@ -4,16 +4,13 @@ class Admin_UserController extends Zend_Controller_Action {
      * ユーザリスト画面
      */
     public function indexAction() {
-//            $muser=new Default_Model_Account;
-//            $data=$muser->listAll();
-//            print_r($data);
-        echo 'Account Controller, index action';
+        
     }
     
     /**
      * ユーザ情報画面
      */
-    public function userInfoAction() {
+    public function infoAction() {
         
     }
     
