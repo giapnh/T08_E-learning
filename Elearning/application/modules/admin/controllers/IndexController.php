98f15620879866403b2c3c65ea34f17d099ffac8
tminhhp <tminhhp@gmail.com>
2014-03-11 15:24:43 +0700

    Admin index

--- Elearning/application/modules/admin/controllers/IndexController.php
+++ Elearning/application/modules/admin/controllers/IndexController.php
@@ -4,7 +4,6 @@ require_once '/../../default/controllers/Message.php';
 require_once 'AccountController.php';
 
 class Admin_IndexController extends IController {
-    
     public function preDispatch() {
         $auth = Zend_Auth::getInstance();
         if ($auth->hasIdentity()) {
@@ -13,6 +12,8 @@ class Admin_IndexController extends IController {
                 if ($this->_request->getActionName() != 'login') {
                     $this->_redirect('admin/account/login');
                 }
+            } else {
+                $this->view->currentUser = $data;
             }
         } elseif ($this->_request->getActionName() != 'login') {
             $this->_redirect('admin/account/login');
@@ -31,6 +32,13 @@ class Admin_IndexController extends IController {
      * 
      */
     public function paymentInfoAction() {
+        $modelLearn = new Admin_Model_Learn();
+        $paymentInfos = $modelLearn->getTotalPaymentInfo();
+        //Zend_Debug::dump($paymentInfos);
+        $this->view->paymentInfos = $paymentInfos;
+        $modelMaster = new Admin_Model_Master();
+        $master = $modelMaster->getMasterData();
+        $this->view->price = $master[Admin_Model_Master::$KEY_COMA_PRICE];
         
     }
     
@@ -40,7 +48,43 @@ class Admin_IndexController extends IController {
      * @param int @month 月
      */
     public function monthPaymentInfoAction() {
-        
+    	$param = $this->getAllParams();
+    	
+    	if(!isset($param["year"]) || !isset($param["month"]))
+    		$this->_redirect('admin/index/payment-info');
+    	if($param["month"] < 10)
+    		$param["month"] = "0".(int)$param["month"];
+    	$this->view->param = $param;
+    	$modelLearn = new Admin_Model_Learn();
+    	$paymentInfos = $modelLearn->getPaymentInfoByMonth($param["month"], $param["year"]);
+    	$this->view->paymentInfos = $paymentInfos;
+    	$teacherPaymentInfos = $modelLearn->getTeacherPaymentInfoByMonth($param["month"], $param["year"]);
+    	//Zend_Debug::dump($teacherPaymentInfos);
+    	$this->view->teacherPaymentInfos = $teacherPaymentInfos;
+    	$modelMaster = new Admin_Model_Master();
+    	$master = $modelMaster->getMasterData();
+    	$this->view->price = $master[Admin_Model_Master::$KEY_COMA_PRICE];
+    	if(isset($param["download"])){
+    		// create TSV file
+    	$filename = "/public/TSV/file-".date("Y-m-d-H-i-s", time()).".tsv";
+   		$path = APPLICATION_PATH."/..".$filename;
+   		$file=fopen($path, "w");
+   		$write=fwrite($file,"ELS-UBT-GWK54M78\t");
+   		$write=fwrite($file,$param["year"]."\t".$param["month"]."\t");
+   		$write=fwrite($file,date("Y\tm\td\tH\ti\ts", time())."\t");
+   		$write=fwrite($file,$this->view->currentUser["username"]."\t");
+   		$write=fwrite($file,$this->view->currentUser["username"]."\r\n");
+   		foreach ($paymentInfos as $info):
+   			$write=fwrite($file,$info["username"]."\t".$info["name"]."\t".($info["total"] * $this->view->price)."\t".$info["address"]."\t".$info["phone"]."\t18\t".$info["bank_account"]. "\r\n");
+   		endforeach;
+   		foreach ($teacherPaymentInfos as $info):
+   			$write=fwrite($file,$info["username"]."\t".$info["name"]."\t".($info["total"] * $this->view->price *0.6 )."\t".$info["address"]."\t".$info["phone"]."\t54\t".$info["bank_account"]. "\r\n");
+   		endforeach;
+   		$write=fwrite($file,"END___END___END\t");
+   		$write=fwrite($file,$param["year"]."\t".$param["month"]);
+   		fclose($file);
+   		$this->_redirect($filename);
+    	}
     }
     
     /**
