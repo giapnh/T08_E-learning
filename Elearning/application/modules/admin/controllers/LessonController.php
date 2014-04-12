3ed05f933abbe6b178c2d22be6874b3de9619411
tminhhp <tminhhp@gmail.com>
2014-04-12 13:37:00 +0700

    admin lesson html

--- Elearning/application/modules/admin/controllers/LessonController.php
+++ Elearning/application/modules/admin/controllers/LessonController.php
@@ -59,7 +59,31 @@ class Admin_LessonController extends IController {
      * 授業を見る画面
      */
     public function lessonAction() {
-        
+        $lessonId = $this->_request->getParam('lesson_id');
+           $lessonModel = new Default_Model_Lesson();
+           $fileModel = new Default_Model_File();
+           $lessonTagModel = new Default_Model_LessonTag();
+           $commentModel = new Default_Model_Comment();
+           $learnModel = new Default_Model_Learn();
+
+           $comment = $this->getParam('comment');
+           if (isset($comment) && $comment != '') {
+                   $commentModel->addComment($lessonId, $this->currentTeacherId, $comment);
+           }
+
+           $lesson = $lessonModel->findLessonById($lessonId);
+           $files = $fileModel->getFileByLesson($lessonId);
+           $tags = $lessonTagModel->getTagsByLesson($lessonId);
+           $comments = $commentModel->getAllCommentOfLesson($lessonId);
+           $studentsNum = $learnModel->countStudenJoinLesson($lessonId);
+           $lesson['students_num'] = $studentsNum;
+           $this->view->lessonId = $lessonId;
+           $this->view->lessonInfo = $lesson;
+           $this->view->files = $files;
+           $this->view->tags = $tags;
+           $this->view->comments = $comments;
+           $this->view->errorMessages = $this->_helper->FlashMessenger->getMessages('addFileFailed');
+           $this->view->messages = $this->_helper->FlashMessenger->getMessages('addFileSuccess');
     }
     
     /**
@@ -67,6 +91,46 @@ class Admin_LessonController extends IController {
      */
     public function fileAction() {
         
+        $lessonModel = new Default_Model_Lesson();
+        $lessonFileModel = new Default_Model_LessonFile();
+        $filecommentModel = new Default_Model_FileComment();
+        $reportModel = new Default_Model_CopyrightReport();
+
+        if ($this->_request->isGet()) {
+            $lessonId = $this->_request->getParam('lesson_id');
+            $currentFileId = $this->_request->getParam('file_id');
+        }
+        if ($this->_request->isPost()) {
+            $u = Zend_Auth::getInstance()->getStorage()->read();
+            $lessonId = $this->_request->getParam('lesson_id');
+            $currentFileId = $this->_request->getParam('file_id');
+            $comment = $this->_request->getParam('comment');
+            if ($comment != NULL) {
+
+                $filecommentModel->addComment($currentFileId, $u['id'], $comment);
+            }
+        }
+        $this->view->reports = $reportModel->getReport($currentFileId);
+        $this->view->comments = $filecommentModel->getAllCommentOfFile($currentFileId);
+        $currentFile = $lessonFileModel->findFileById($currentFileId);
+        $lessonInfo = $lessonModel->findLessonById($lessonId);
+        $this->view->lessonInfo = $lessonInfo;
+        $files = $lessonFileModel->listFileOfLesson($lessonId);
+        $this->view->files = $files;
+        if ($currentFile == NULL) {
+            $this->view->currentFile = $files[0];
+        } else {
+            $this->view->currentFile = $currentFile;
+        }
+
+        $this->view->fileModel = new Default_Model_File();
+        $this->view->controller = $this;
+        $this->view->lessonId = $lessonId;
+    }
+    
+    public function getTestHtml($testId) {
+        $fileModel = new Default_Model_File();
+        return $fileModel->getTestHtml($testId);
     }
     
     /**
