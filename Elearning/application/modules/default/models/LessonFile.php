3edd9a3a2f5f2f8e443dff45b8821fb383f066a1
saotroidenkhuya162 <thienbk4c@gmail.com>
2014-04-13 16:00:52 +0700

    checkUserCanSeeFile

--- Elearning/application/modules/default/models/LessonFile.php
+++ Elearning/application/modules/default/models/LessonFile.php
@@ -21,5 +21,15 @@ class Default_Model_LessonFile extends Zend_Db_Table_Abstract {
                 ->where("lesson_id= ?", $lessonId);
         return $this->getAdapter()->fetchAll($select);
     }
+    //thiennx check user can see the file
+    public function checkUserCanSeeFile($userId, $fileId){
+    	$select = $this->getAdapter()->select()
+    			->from($this->_name)
+    			->join("learn", "learn.lesson_id = lesson_file.lesson_id")
+    			->where("lesson_file.id = ?", $fileId)
+    			->where("student_id = ?", $userId)
+    			->where("learn.register_time + INTERVAL 7 DAY >= NOW() ");
+    	return $this->getAdapter()->fetchAll($select);
+    }
 
 }
