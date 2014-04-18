6d25230a0c291cb04863522e2f9bb8afbb9ace3e
tminhhp <tminhhp@gmail.com>
2014-04-19 00:06:17 +0700

    Admin view file

--- Elearning/application/modules/admin/controllers/LessonController.php
+++ Elearning/application/modules/admin/controllers/LessonController.php
@@ -122,6 +122,38 @@ class Admin_LessonController extends IController {
         $this->view->lessonId = $lessonId;
     }
     
+    public function streamAction() {
+        $fileId = $this->_request->getParam("id");
+        $lessonFileModel = new Default_Model_File();
+        $file = $lessonFileModel->findFileById($fileId);
+        $path = $lessonFileModel->getFileFolder() . $file["location"];
+        
+        $currentFileExt = explode(".", $file['filename']);
+        $currentFileExt = $currentFileExt[count($currentFileExt)-1];
+        $arrayType = array(
+            "pdf" => "application/pdf",
+            "mp3" => "audio/mpeg",
+            "mp4" => "video/mp4",
+            "jpg" => "image/jpeg",
+            "png" => "image/jpeg",
+            "gif" => "image/jpeg",
+            "wav" => "audio/mpeg"
+        );
+        
+        if (is_readable($path)) {
+            if ($currentFileExt == "pdf") {
+                echo    $link = $this->view->serverUrl() . $this->view->baseUrl() . "/public/viewpdf/web/viewer.html?file=" 
+                        . $this->view->serverUrl() . $this->view->baseUrl() . "/" . $lessonFileModel->getFileFolderName() . $file["location"];
+                header("Location: " . $link);
+            } else {
+                header('Content-type: ' . $arrayType[$currentFileExt]);
+                header("Content-Length: " . filesize($path));
+                readfile($path);
+            }
+        }
+        exit();
+    }
+    
     /**
      * テストファイルからHtml文字列を取る
      * 
