c8829f700a1ec065856e08753dfe13ec483689a3
saotroidenkhuya162 <thienbk4c@gmail.com>
2014-04-24 23:08:12 +0700

    reset form when back

--- Elearning/application/modules/admin/controllers/IController.php
+++ Elearning/application/modules/admin/controllers/IController.php
@@ -7,6 +7,7 @@ class IController extends Zend_Controller_Action {
         $this->view->headtitle("E-learning");
         $this->view->headMeta()->appendName("keyword", "E-learning...");
         $this->view->headLink()->appendStylesheet($baseurl . "/public/css/admin-common-style.css");
+        $this->view->headScript()->appendFile($baseurl . "/public/js/jquery.min.js");
     }
 
 }
